22347861332ccefa0dbe8b13b3434de6402bc6df
rtanmay <rtanmay05@gmail.com>
2018-01-28 13:01:46 +0530

    Added program to calculate space saved

---
+++ calcspace.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	cout<<"Enter the order of matrix (N):";
+	unsigned long long int N;
+	cin>>N;
+	cout<<"Enter the size of the data elements(bytes):";
+	int s;
+	cin>>s;
+	double c;
+	c=s*(N*N-N)/2;
+	cout<<"The space stored by storing only upper or lower traingle instead of the whole matrix is: "<<c<<" bytes\n";
+	return 0;
+
+}
