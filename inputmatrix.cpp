401089784d0eadec3b5a7760e28cfa014d7c49a8
rtanmay <rtanmay05@gmail.com>
2018-01-28 21:25:18 +0530

    added templates

--- inputmatrix.cpp
+++ inputmatrix.cpp
@@ -1,23 +1,57 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//The class is created similar to matrix template in Eigen
+
+template <typename T>
+class Symtrix                       
+    {   
+      public:
+       int N;
+       int S;
+       S=(N*N+N)/2;
+       T M[n][n];
+       Symtrix(int initialise)        //CONSTRUCTOR
+       {
+       	N=initialise;
+       }
+       T M[100][100];
+       void Input(int);
+       void Print(int);
+
+    };
+
+void Symtrix::Input(int n)
+{ 
+  int i,j;
+  for(i=0;i<n;i++)
+  {
+  	for(j=0;j<n;j++)
+  	{
+  		cin>>M[i][j];
+  	}
+  }
+}
+void Symtrix::Print(int n)
+{
+  int i,j;
+  cout<<"----------------------------------";
+  for(i=0;i<n;i++)
+  {
+  	for(j=0;j<n;j++)
+  	{
+  		cout<<M[i][j];
+  	}
+  }
+
+}
 int main()
 {   
-	cout<<"Enter the order of the matrix(N):";
-	unsigned long long int N;
-	cin>>N;
-    double M[N][N];
-    int i,j;
-    for(i=0;i<N;i++)
-    {
-    	cout<<"Enter the "<<i+1<<"th row:";
-    	for(j=0;j<N;j++)
-    	{
-          cin>>M[i][j];
-    	}
-    }
-    cout<<"Matrix has been entered!\n";
-
-
-	return 0;
+	int num;
+	cout<<"Enter the order of the matrix(less than 10^8):";
+	cin>>num;
+	Symtrix<float> s(num);
+	s.Input(s.N);
+	s.Print(s.N);
+    return 0;	
 }
